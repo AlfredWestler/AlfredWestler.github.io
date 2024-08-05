a71b87305e16e8a13889bc452472af994a931668
fredo granciano <fredo.granciano@gmail.com>
2024-08-05 10:20:32 -0600

    prueba mapa

--- wedding/scripts/mapsUrlScript.js
+++ wedding/scripts/mapsUrlScript.js
@@ -2,3 +2,4 @@ const churchUrl = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAsl_wF89
 const eventUrl = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAsl_wF89EpRrIYJ1-nxn70zPk_Jt2SpuQ&q=Salones+de+Eventos+Cabaña+Encantada"
 document.getElementById("church-maps-frame").src = churchUrl;
 document.getElementById("event-maps-frame").src = eventUrl;
+document.getElementById("example_map").src = churchUrl;
