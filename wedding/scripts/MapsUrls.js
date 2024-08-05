273d5fee36bcf3d539e1c01fc2eba15c788ee88d
fredo granciano <fredo.granciano@gmail.com>
2024-08-05 12:17:55 -0600

    new maps config

--- wedding/scripts/MapsUrls.js
+++ wedding/scripts/MapsUrls.js
@@ -1,5 +1,4 @@
 const churchUrl = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAsl_wF89EpRrIYJ1-nxn70zPk_Jt2SpuQ&q=Rectoría+San+Buenaventura"
 const eventUrl = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAsl_wF89EpRrIYJ1-nxn70zPk_Jt2SpuQ&q=Salones+de+Eventos+Cabaña+Encantada"
-document.getElementById("church-maps-frame").src = churchUrl;
-document.getElementById("event-maps-frame").src = eventUrl;
-document.getElementById("example_map").src = churchUrl;
+document.getElementById("event_maps_frame").src = eventUrl;
+document.getElementById("church_maps_frame").src = churchUrl;
